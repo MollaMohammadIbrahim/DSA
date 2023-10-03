49f83c362d79000fc187ccd58e4609bf072e4db7
MollaMohammadIbrahim <mmibrahim.cseru@gmail.com>
2023-10-03 18:19:45 +0600

    m

--- LInkedList/linklist.cpp
+++ LInkedList/linklist.cpp
@@ -43,11 +43,49 @@ void print()
 			current_node=current_node->next;
 		}
 }
+
+void delete_node(int data){
+    Node *curr_node, *prev_node;
+    curr_node = root;
+    
+    //seraching for node which to be deleted
+    while(curr_node -> data != data and curr_node -> next != NULL  ){
+        prev_node = curr_node;
+        curr_node = curr_node->next;
+    }
+    if(curr_node->next == NULL and curr_node->data != data){
+        cout<<"No data found"<<endl;
+        return;
+    }
+    // if current node and and root node are same then updating the root node to root -> next
+    if(curr_node == root){
+        cout<<"Deleted "<<curr_node->data<<endl;
+        Node *temp = root;
+        root = root->next;
+        delete(temp);
+    }
+    
+    // if current node is not root then update the previous node pointer to current node next pointer
+    else{
+        
+        prev_node->next = curr_node->next;
+        cout<<"Deleted "<<curr_node->data<<endl;
+        delete(curr_node);
+    }
+}
+
 int main(){
 
 	append(1);
 	append(2);
 	append(6);
+    delete_node(1);
 	print();
+    delete_node(2);
+    print();
+    delete_node(6);
+    print();
+    delete_node(7);
+    print();
     return 0;
 }
