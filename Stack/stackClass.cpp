2225b3d4e6faa6c8e6a66f5e415f1895606ffe7a
MollaMohammadIbrahim <mmibrahim.cseru@gmail.com>
2023-10-03 18:19:56 +0600

    m

--- Stack/stackClass.cpp
+++ Stack/stackClass.cpp
@@ -55,7 +55,17 @@ public:
         return front == capacity - 1;
     }
 };
+Node* findMiddle(Node* head) {
+    Node* slow = head;
+    Node* fast = head;
 
+    while (fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
 int main() {
     // Create a stack with a capacity of 5
     Stack myStack(5);
